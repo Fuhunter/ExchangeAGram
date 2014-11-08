8d75be99de8c89289a2738c4621a04a349c3e4d7
Fuhunter <fuhunter@t-online.de>
2014-11-08 17:08:16 +0100

    facebook login

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Niclas Günther on 08.11.14.
+//  Copyright (c) 2014 Niclas Günther. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
---
+++ ExchangeAGram/ProfileViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileViewController.swift
+//  ExchangeAGram
+//
+//  Created by Niclas Günther on 08.11.14.
+//  Copyright (c) 2014 Niclas Günther. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, FBLoginViewDelegate {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var fbLoginView: FBLoginView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "publish_actions"]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
+        profileImageView.hidden = false
+        nameLabel.hidden = false
+    }
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        println(user)
+        
+        nameLabel.text = user.name
+        
+        let userImageURL = "https://graph.facebook.com/\(user.objectID)/picture?type=small"
+        let url = NSURL(string: userImageURL)
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+        profileImageView.hidden = true
+        nameLabel.hidden = true
+    }
+    
+    func loginView(loginView: FBLoginView!, handleError error: NSError!) {
+        println("Error: \(error.localizedDescription)")
+    }
+}
